6166a3bcb952168553a43a123817cda1c8d86324
Adrian Suliga <adriansuliga81@gmail.com>
2025-11-01 17:18:47 +0100

    Generate csv statistics for different variations of the problem

--- philosophers/src/main/java/org/philosophers/Main.java
+++ philosophers/src/main/java/org/philosophers/Main.java
@@ -1,12 +1,23 @@
 package org.philosophers;
 
+import org.philosophers.model.PhilosopherV1;
+import org.philosophers.model.PhilosopherV2;
+import org.philosophers.model.PhilosopherV3;
+import org.philosophers.model.PhilosopherV4;
+import org.philosophers.model.PhilosopherV5;
 import org.philosophers.model.PhilosopherV6;
 import org.philosophers.simulation.*;
 
 public class Main {
-    static void main() {
-        /* Example usage */
-        Simulation<PhilosopherV6> simulation = new Simulation<>(15, PhilosopherV6.class);
+    public static void main() {
+
+        /* Example simulation with N philosophers executing variant 6
+        int N = 20;
+
+        var simulation = new Simulation<>(N, PhilosopherV6.class);
         simulation.simulate();
+         */
+
+
     }
 }
