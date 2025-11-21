5def33e94f05e2f2aa7a98f6011ceeba86ce9a42
Adrian Suliga <adriansuliga81@gmail.com>
2025-11-21 19:41:07 +0100

    Solve traces theory exercises

---
+++ traces_theory/main.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import sys
+import util
+
+def main():
+    if (len(sys.argv)) != 4:
+        print("Unexpected number of arguments. Expected format is:")
+        print("python3 main.py <transaction_file> <alphabet_file> <word>")
+        sys.exit(0)
+
+    transaction_filename = sys.argv[1]
+    alphabet_filename = sys.argv[2]
+    word = sys.argv[3]
+
+    transactions = util.get_transactions(transaction_filename)
+    alphabet = util.get_alphabet(alphabet_filename)
+
+    if not util.verify_input(transactions, alphabet):
+        print("Text files data verification failed")
+        sys.exit(0)
+
+    if not util.verify_word(alphabet, word):
+        print("Word verification failed")
+        sys.exit(0)
+
+    sigma = [(x, y) for y in alphabet for x in alphabet]
+    dependent = sorted(list(util.get_dependent_transactions(transactions)))
+    independent = sorted([item for item in sigma if not item in dependent])
+
+    print("D = {", end = "")
+    for i in range(len(dependent)):
+        if i > 0: print(",", end = "")
+        print(f"{dependent[i]}", end = "")
+    print("}")
+
+    print("I = {", end = "")
+    for i in range(len(independent)):
+        if i > 0: print(",", end = "")
+        print(f"{independent[i]}", end = "")
+    print("}")
+
+    dependencies = util.get_dependencies(dependent, alphabet)
+    fnf = util.fnf_calculation(dependencies, word, alphabet)
+    print(f"FNF([{word}]) = ", end = "")
+    for f in fnf:
+        print("(", end = "")
+        for transaction in sorted(f):
+            print(transaction, end = "")
+        print(")", end = "")
+    print()
+
+    G, positions, labels = util.get_graph(dependencies, word)
+    plt.figure(num = f"Graf zależności dla słowa {word}")
+    nx.draw(G, positions, labels = labels, with_labels = True,
+            node_size = 600)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- traces_theory/util.py
+++ traces_theory/util.py
@@ -1,5 +1,170 @@
+import networkx as nx
 
-def get_transaction(trans_file: str) -> str:
-    with open(trans_file) as file:
+# input - str: transaction.txt path
+# output - dict: map of variables used in a given transaction (symbol -> list of variables used) 
+def get_transactions(transaction_file: str) -> dict[str, list[str]]:
+    with open(transaction_file) as file:
         text = file.read()
+        lines = text.split('\n')
+
+        transactions = {}
+
+        for line in lines:
+            opening_idx = line.find("(")
+            closing_idx = line.find(")")
+
+            transaction_symbol = line[opening_idx + 1 : closing_idx]
+
+            transactions[transaction_symbol] = []
+
+            for idx in range(closing_idx + 1, len(line)):
+                if 'a' <= line[idx] <= 'z':
+                    transactions[transaction_symbol] += line[idx]
+        
+        return transactions
+
+# input - str: alphabet.txt path
+# output - list: alphabet sorted in alphabetical order
+def get_alphabet(alphabet_file: str) -> list[str]:
+    with open(alphabet_file) as file:
+        text = file.read()
+        alphabet = []
+
+        for char in text:
+            if 'a' <= char <= 'z':
+                alphabet += char
+        
+        return sorted(alphabet)
+
+# input - dict: transactions, result of get_transactions
+#       - list: alphabet, result of get_alphabet
+# output - bool: True if data is valid False otherwise
+def verify_input(transactions:dict[str, list[str]], alphabet:list[str]) -> bool:
+    return sorted(list(transactions.keys())) == alphabet
+
+# input - list: alphabet, list of valid transactions
+#       - str: word to check
+# output - bool: True if word is valid False otherwise
+def verify_word(alphabet:list[str], word: str) -> bool:
+    for transaction in word:
+        if transaction not in alphabet:
+            return False
     
+    return True
+
+# input - dict: transactions, result of get_transactions
+# output - set: set of dependent transactions
+def get_dependent_transactions(transactions: dict[str, list[str]]) -> set[tuple[str, str]]:
+    transaction_keys = transactions.keys()
+    result = set()
+
+    for outer_key in transaction_keys:
+        left = transactions[outer_key][0]
+
+        for inner_key in transaction_keys:
+            for var in transactions[inner_key]:
+                if left == var:
+                    # mutually dependent
+                    result.add((outer_key, inner_key))
+                    result.add((inner_key, outer_key))
+                    # no need for further search in the inner loop
+                    break
+    
+    return result
+
+# input - set: dependent transactions, result of get_dependent_transactions
+#       - list: alphabet, result of get_alphabet
+# output - dict: dependent transactions without redundant entries (symbol -> set of symbols)
+def get_dependencies(dependent:set[tuple[str, str]], alphabet:list[str]) -> dict[set[str]]:
+    result = {}
+
+    for transaction in alphabet:
+        result[transaction] = set()
+
+    for first, second in dependent:
+        if first is not second:
+            result[first].add(second)
+            result[second].add(first)
+    
+    return result
+
+# helper function for fnf algorithm
+def get_trans(trans_dict: dict[str, list[int]]) -> list[str]:
+    result = []
+
+    for key in trans_dict.keys():
+        if trans_dict[key] and trans_dict[key][-1] == 1:
+            trans_dict[key].pop()
+            result.append(key)
+    
+    return result
+
+# input - dict: dependencies, result of get_dependencies
+#       - str: word to calculate fnf for
+#       - list: alphabet, result of get_alphabet
+# output - list: fnf for given word
+def fnf_calculation(dependencies: dict[set[str]], word: str, alphabet: list[str]) -> list:
+    trans_dict = {}
+    for transaction in alphabet:
+        trans_dict[transaction] = []
+
+    for transaction in word[::-1]:
+        trans_dict[transaction].append(1)
+
+        for dep_trans in dependencies[transaction]:
+            trans_dict[dep_trans].append(0)
+    
+    fnf = []
+
+    while True:
+        trans_list = get_trans(trans_dict)
+
+        if not trans_list: break
+
+        fnf.append(trans_list)
+
+        for transaction in trans_list:
+            for dependency in dependencies[transaction]:
+                trans_dict[dependency].pop()
+    
+    return fnf
+
+# input - dict: dependencies, result of get_dependencies
+#       - str: word
+# output - tuple: Graph, Positions and Labels
+def get_graph(dependencies:dict[set[str]], word: str) -> tuple[nx.DiGraph, dict[str, list[int]], dict[str, str]]:
+    edges = set()
+
+    for i in range(len(word) - 1):
+        for j in range(i + 1, len(word)):
+            if word[j] in dependencies[word[i]] or word[i] == word[j]:
+                edges.add((i + 1, j + 1))
+
+    for i in range(1, len(word) - 1):
+        for j in range(i + 1, len(word)):
+            if (i, j) in edges:
+                for k in range(j + 1, len(word) + 1):
+                    if (i, k) in edges and (j, k) in edges:
+                        edges.remove((i, k))
+
+    G = nx.DiGraph()
+
+    mappings = {}
+    for i in range(len(word)):
+        G.add_node(f"{word[i]}{i}")
+        mappings[i + 1] = f"{word[i]}{i}"
+    
+    converted_edges = set()
+    for u, v in edges:
+        converted_edges.add((mappings[u], mappings[v]))
+
+    G.add_edges_from(converted_edges)
+    positions = {}
+    labels = {}
+
+    for i in range(len(word)):
+        positions[f"{word[i]}{i}"] = [(-1) ** (i + 1), 1 - i // 2]
+        labels[f"{word[i]}{i}"] = word[i]
+
+    return G, positions, labels
+
